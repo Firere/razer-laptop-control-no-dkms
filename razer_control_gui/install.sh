6111327d614a4007efa628c343359bd0bce41f15
Firere <firere@firere.net>
2022-08-24 16:10:14 +0200

    New logo (again)

--- razer_control_gui/install.sh
+++ razer_control_gui/install.sh
@@ -17,6 +17,8 @@ cp src/gui.py /usr/bin/razercontrol-gui
 chmod +x /usr/bin/razercontrol-gui
 mv razercontrol.desktop /usr/share/applications/razercontrol.desktop
 mv assets/logo1.png /usr/share/razercontrol/logo1.png
+mv assets/logo2.png /usr/share/razercontrol/logo2.png
+mv assets/logo3.ong /usr/share/razercontrol/logo3.png
 EOF
 systemctl --user enable razerdaemon.service
 systemctl --user start razerdaemon.service
